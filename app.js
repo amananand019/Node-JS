88952a1e6e0831616fca87f41c9c651eb01f3c6b
amananand019 <64424809+amananand019@users.noreply.github.com>
2021-09-09 18:59:57 +0530

    module-alternative-flavor

--- app.js
+++ app.js
@@ -2,8 +2,14 @@
 // modules - Encapsulated Code (only share minimum)
 const names = require('./4-names')
 const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavor')
 
 console.log(names);
+console.log(data);
+
 sayHi('aman')
 sayHi(names.john)
 sayHi(names.peter)
+sayHi(data.items[0])
+sayHi(data.items[1])
+sayHi(data.person.name)
